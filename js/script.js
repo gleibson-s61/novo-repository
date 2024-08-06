1fa67ec1e91f294291a30e29d25cea11d203b48f
GLEIBSON-PROJET <gleibson.silva.2022@aluno.etegec.net.br>
2024-08-06 20:20:12 -0300

    teste. javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-const nome = "josé";
-const sobrenome = "Gleibson";
+let usuario = prompt("Digite seu nome completo.");  
+    let  nomeCompleto = document.getElementById('titulo');
+        nomeCompleto.innerHTML = (`<br><strong>${usuario}</strong>`);
 
-window.alert(`Senja bem vindo "${nome} ${sobrenome}" `)
+let texto = document.getElementById('Usuario-1');
+texto.innerHTML += (`<br> Seja bem vindo: ${usuario}`);
+texto.innerHTML += (`<br> Senhor: ${usuario} voçê tem <strong>32</strong> anos de idade`);
+texto.innerHTML += (`<br> ${usuario} voçê tem <strong>1.75</strong> de altura`);
+texto.innerHTML += (`<br>  ${usuario} voçê tem uma nova jornada para Enfrentar!`);
+texto.innerHTML += (`<br> senhor ${usuario} sua <strong>Graduação</strong> ja se enconta Disponivel no sistema.`);
+texto.innerHTML += (`<br>Senhor ${usuario} <strong>Jesus</strong> faz o <strong>impossivel</strong> ancontecer em sua vida!`);
